a67d18d8d8ddb1bc09db1686b75e519b3706d601
Layo Folaranmi <thedarkkid.codes@gmail.com>
2022-03-02 00:26:58 +0100

    Implemented a queue ds

---
+++ ds/Queue.js
@@ -0,0 +1,40 @@
+const { Node, LinkedList } = require('./LinkedList');
+
+class Queue {
+  head;
+  tail;
+
+  add (item) {
+    if (!item) return;
+    if (!this.head || !this.tail) {
+      this.tail = new Node(item);
+      this.head = this.tail;
+      return;
+    }
+    this.tail.next = new Node(item);
+    this.tail = this.tail.next;
+  }
+
+  remove () {
+    if (!this.tail || !this.head) return null;
+    const headValue = this.head.value;
+    this.head = this.head.next;
+    return headValue;
+  }
+
+  peek () {
+    if (!this.head) return null;
+    return this.head.value;
+  }
+
+  isEmpty () {
+    return !!(!this.tail || !this.head);
+  }
+
+  visual () {
+    return LinkedList.toArray(this.head).reverse();
+  }
+}
+
+
+module.exports = { Queue };
