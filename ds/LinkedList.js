c93cacb04c798f0c5b5f296525dd569edeffa0a1
Layo Folaranmi <thedarkkid.codes@gmail.com>
2022-03-02 00:26:22 +0100

    created linked list DS

---
+++ ds/LinkedList.js
@@ -0,0 +1,39 @@
+class Node {
+  value;
+  next;
+
+  constructor (val) {
+    this.value = val;
+  }
+}
+
+class LinkedList {
+  head;
+
+  constructor (arr) {
+    this.fromArray(arr);
+  }
+
+  fromArray (arr) {
+    if (!arr.length || arr.length <= 0) return;
+    const head = new Node(arr[0]);
+    let current = head;
+    for (let i = 1; i < arr.length; i++) {
+      current.next = new Node(arr[i]);
+      current = current.next;
+    }
+    this.head = head;
+  }
+
+  static toArray (current) {
+    const arr = [];
+    while (current) {
+      arr.push(current.value);
+      current = current.next;
+    }
+    return arr;
+  }
+
+}
+
+module.exports = { LinkedList, Node };
