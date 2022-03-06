ccc9bd804591ba8291cf2aee495d51e184cee0de
Layo Folaranmi <thedarkkid.codes@gmail.com>
2022-03-06 10:04:56 +0100

    added breadth-first-search and depth-first-search algorithms

--- algo/binaryTrees.js
+++ algo/binaryTrees.js
@@ -1,3 +1,5 @@
+const { Queue } = require('../ds/Queue');
+
 const visitNode = (node) => {
   console.log(node);
 };
@@ -21,4 +23,30 @@ const rPostOrderTraversal = (node) => {
   rPostOrderTraversal(node.left);
   rPostOrderTraversal(node.right);
   visitNode(node);
+};
+
+const bfs = (startNode, value) => {
+  const isVisited = new Map();
+  const queue = new Queue();
+  queue.add(startNode);
+
+  while (!queue.isEmpty()) {
+    const current = queue.remove();
+    if (isVisited.has(current.value)) continue;
+    if (current.value === value) return current;
+    queue.addMany(current.children);
+    isVisited.set(current, true);
+  }
+
+  return false;
+};
+
+const dfs = (currentNode, value, isVisited = new Map()) => {
+  if (isVisited.has(currentNode)) return false;
+  if (currentNode.value === value) return true;
+  isVisited.set(currentNode.value, true);
+
+  for (const childNode of currentNode.children) if (dfs(childNode, value, isVisited)) return childNode;
+
+  return false;
 };
